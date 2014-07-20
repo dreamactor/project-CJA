e6b6979b15756c7f5ec09fd04c74e6a5cedc2b33
yixiong <yixiong@umich.edu>
2014-07-20 10:11:57 -0400

    handle numbers in a file

--- mean.py
+++ mean.py
@@ -1,9 +1,12 @@
+#print the mean of numbers given in a file
+
 import sys
+
 sum = 0
 n = 0
 
 # test
-for num in sys.stdin:
+for num in open('data.txt):
     sum += folat(num)
       n += 1
 print sum/n 
